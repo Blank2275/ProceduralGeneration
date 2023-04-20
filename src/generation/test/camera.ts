bccd6c4f5beff253ad5472a529d4d557da777dde
Connor Reed <Cfr3.14156@gmail.com>
2023-04-19 21:01:23 -0600

    added camera controls

--- src/generation/test/camera.ts
+++ src/generation/test/camera.ts
@@ -1,20 +1,42 @@
 import { Camera } from "../../display/display";
-class CameraManager{
+export class CameraManager{
     camera: Camera;
     element: HTMLElement;
     down: number[];
-    constructor(camera: Camera, element: HTMLElement){
+    speed: number;
+    constructor(camera: Camera, element: HTMLElement, speed: number){
         this.camera = camera;
         this.element = element;
         this.down = [];
+        this.speed = speed;
 
-        this.element.addEventListener("keydown", this.keyDown);
-        this.element.addEventListener("keyup", this.keyUp);
+        this.element.addEventListener("keydown", (e) => this.keyDown(e));
+        this.element.addEventListener("keyup", (e) => this.keyUp(e));
     }
-    keyDown(){
+    keyDown(e){
+        console.log(e.keyCode)
+        if(!this.down.includes(e.keyCode)){
+            this.down.push(e.keyCode);
+        }
+    }
+    keyUp(e){
+        if(this.down.includes(e.keyCode)){
+            this.down.splice(this.down.indexOf(e.keyCode));
+        }
+    }
 
-    }
-    keyUp(){
-        
+    update(){
+        if(this.down.includes(38)){
+            this.camera.y -= this.speed;
+        }
+        if(this.down.includes(39)){
+            this.camera.x += this.speed;
+        }
+        if(this.down.includes(40)){
+            this.camera.y += this.speed;
+        }
+        if(this.down.includes(37)){
+            this.camera.x -= this.speed;
+        }
     }
 }
--- src/generation/test/index.ts
+++ src/generation/test/index.ts
@@ -2,6 +2,7 @@ import { DisplayManager, Camera } from "../../display/display";
 import { colors } from "../../assets/test/colors";
 import { entities } from "../../assets/test/images";
 import { generateTerrain } from "./generate";
+import { CameraManager } from "./camera";
 let colorArray = Object.values(colors);
 let entityArray = Object.values(entities);
 
@@ -23,11 +24,13 @@ let grid = generateTerrain(100, 100);
 let canvas = manager.genCanvas(window.innerWidth / 1.1, window.innerHeight / 1.1, 4);
 document.body.appendChild(canvas);
 
+let cameraManager = new CameraManager(camera, document.body, 0.25);
+
 animate();
 
 function animate(){
-    camera.x = Math.cos(new Date().getTime() / 1000) * 8 + 8;
-    camera.y = Math.sin(new Date().getTime() / 1000) * 8 + 8;
+    cameraManager.update()
+
     manager.clear();
     manager.drawGrid(grid, 30, colorArray, entityArray);
     requestAnimationFrame(animate);
